c903e010624aabd5557ea61e553794c558250e6a
Elias Bello <eabelloc.dev@gmail.com>
2022-07-26 08:57:54 +0200

    Neoland-ES6_Ejercicios-hasta-5-punto-5_Listo

--- app.js
+++ app.js
@@ -2,58 +2,283 @@
 /*Crea una arrow function que tenga dos parametros a y b y 
 que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
 por consola la suma de los dos parametros.*/
-const sum = (a, b) => {
-    console.log(a + b)
-}
 
-sum(5, 5)
+let a = 10
+let b = 5
+
+const sum = (a, b) => {
+    console.log(a + b);
+};
+
+sum(a, b);
 
 //1.1 Ejecuta esta función sin pasar ningún parametro
 const sum1 = () => {
-    console.log()
+    console.log(a + b);
 }
-sum1()
+sum1();
 
 //1.2 Ejecuta esta función pasando un solo parametro
 const sum2 = (a) => {
-    console.log(a)
+    console.log(a + b);
 }
 
-sum2(5)
+sum2(a);
 
 //1.3 Ejecuta esta función pasando dos parametros
 const sum3 = (a, b) => {
-    console.log(a + b)
-}
+    console.log(a + b);
+};
 
-sum(5, 5)
-/*-------------------------------------------------------------------*/
+sum3(a, b);
 
+console.log(/---------------------------------------------------------------------/);
+/*---------------------------------------------------------------------*/
 /*Iteración #2: Destructuring*/
 
 /*2.1 En base al siguiente javascript, crea variables en base a las propiedades 
 del objeto usando object destructuring e imprimelas por consola. Cuidado, 
 no hace falta hacer destructuring del array, solo del objeto.*/
 
-const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+const game = {title: 'The last us 2', genre: ['action', 'zombie', 'survival'], year: 2020};
 
+const {title, genre, year} = game;
+console.log(title, genre, year);
 
 /*2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 imprimelo por consola.*/
 
 const fruits = ['Banana', 'Strawberry', 'Orange'];
+const [fruit1, fruit2, fruit3] = fruits;
+
+console.log(fruit1, fruit2, fruit3);
 
 /*2.3 En base al siguiente javascript, usa destructuring para crear 2 
 variables igualandolo a la función e imprimiendolo por consola.*/
 
 const animalFunction = () => {
-    return {name: 'Bengal Tiger', race: 'Tiger'}
+    return {nombre: 'Bengal Tiger', race: 'Tiger'};
 };
+
+const {nombre} = animalFunction();
+const {race} = animalFunction();
+
+console.log(nombre);
+console.log(race);
 
 /*2.4 En base al siguiente javascript, usa destructuring para crear las 
 variables name y itv con sus respectivos valores. Posteriormente crea 
 3 variables usando igualmente el destructuring para cada uno de los años 
 y comprueba que todo esta bien imprimiendolo.*/
 
-const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+const car = {nombre1: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+const {nombre1} = car;
+const {itv} = car;
+const [year1, year2, year3] = car.itv;
+
+console.log(nombre1, itv, year1, year2, year3);
+
+
+console.log(/---------------------------------------------------------------------/)
+/*---------------------------------------------------------------------*/
+
+/**Iteración #3: Spread Operator**/
+/*3.1 Dado el siguiente array, crea una copia usando spread operators.*/
+const pointsList = [32, 54, 21, 64, 75, 43];
+const myPointsList = [...pointsList];
+
+console.log(myPointsList);
+
+/*3.2 Dado el siguiente objeto, crea una copia usando spread operators.*/
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const myToy = {...toy};
+
+console.log(myToy);
+
+/*3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
+spread operatos.*/
+const pointsList1 = [32, 54, 21, 64, 75, 43];
+const pointsList2 = [54,87,99,65,32];
+
+const manyPointsList = [...pointsList1, ...pointsList2]
+console.log(manyPointsList);
+
+/*3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
+con spread operators.*/
+const toy1 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']};
+
+const myNewToy = {...toy1, ...toyUpdate};
+console.log(myNewToy);
+
+/*3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
+pero sin editar el array inicial. De nuevo, usando spread operators.*/
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const newColors = [...colors];
+newColors.splice(2,1);
+console.log(newColors);
+
+
+console.log(/---------------------------------------------------------------------/)
+/*---------------------------------------------------------------------*/
+
+/*Iteración #4: Map*/
+
+/*4.1 Dado el siguiente array, devuelve un array con sus nombres
+utilizando .map().*/
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const onlyNames = users.map(users => users.name)
+
+console.log(onlyNames)
+
+/*4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
+empiece por 'A'.*/
+const users1 = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const onlyNmaes1 = users1.map(users1 => (users1.name.startsWith('A') == true)? 'Anacleto' : users1.name);
+console.log(onlyNmaes1);
+
+
+/*4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+cuando el valor de la propiedad isVisited = true.*/
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+
+const citiesToGo = cities.map(cities => (cities.isVisited === true)? cities.name += ' (Visitado)': cities.name);
+console.log(citiesToGo);
+
+
+console.log(/---------------------------------------------------------------------/);
+/*---------------------------------------------------------------------*/
+
+/*Iteración #5: Filter*/
+
+/*5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+con los valores que sean mayor que 18*/
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const agesAbove = ages.filter(ages => ages > 18);
+console.log(agesAbove);
+
+/*5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+con los valores que sean par.*/
+const ages1 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const agesEven = ages1.filter(ages1 => ages1 % 2 === 0);
+console.log(agesEven);
+
+/*5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+con los streamers que tengan el gameMorePlayed = 'League of Legends'.*/
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const lol = streamers.filter(streamers => streamers.gameMorePlayed === 'League of Legends');
+console.log(lol);
+
+/*5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
+usar la funcion .includes() para la comprobación.*/
+const streamers1 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const amongUs = streamers1.filter(streamers1 => (streamers1.name.includes('u') === true)? streamers1.name : "");
+console.log(amongUs);
+
+/*5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+.includes() para la comprobación.
+Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+.age sea mayor que 35.*/
+
+const legends = streamers1.filter((streamers1) => {
+    const returnLegends = streamers1.gameMorePlayed.toLowerCase().includes("legends");
+    
+    if (returnLegends && streamers1.age > 35) {
+        streamers1.gameMorePlayed = streamers1.gameMorePlayed.toUpperCase();
+    } return returnLegends;
+});
+console.log(legends);
+
+/*5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.*/
+const streamers2 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+
+/*<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+             <meta http-equiv="X-UA-Compatible" content="ie=edge">
+             <title>Document</title>
+</head>
+<body>
+  <input type="text" data-function="toFilterStreamers"/>
+</body>
+</html></meta>*/
+
+/*5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', 
+me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.*/
+const streamers3 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+/*<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+             <meta http-equiv="X-UA-Compatible" content="ie=edge">
+             <title>Document</title>
+</head>
+<body>
+  <input type="text" data-function="toFilterStreamers"/>
+  <button data-function="toShowFilterStreamers">Filter</button>
+</body>
+</html>*/
+
+
+
+// ARRAY = [] = CORCHETES [LISTAS DESORDENADAS]
+// OBJETOS = {} = LLAVES {LISTAS ORDENADAS}
